c198ca78ddd4a956cdcfc4f456967b05235b857a
syfxlin <syfxlin@gmail.com>
2019-07-18 00:08:22 +0800

    Fix waring

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -128,13 +128,13 @@ p {
 ::-webkit-scrollbar-track {
   border-radius: 3px;
   background: rgba(0, 0, 0, 0.06);
-  -webkit-box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.08);
+  box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.08);
 }
 /* 滚动条滑块 */
 ::-webkit-scrollbar-thumb {
   border-radius: 3px;
   background: rgba(0, 0, 0, 0.12);
-  -webkit-box-shadow: inset 0 0 10px rgba(0, 0, 0, 0.2);
+  box-shadow: inset 0 0 10px rgba(0, 0, 0, 0.2);
 }
 #app {
   width: 100%;
