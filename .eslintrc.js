0750c5e29bd38741d39d973117197badbcf05064
syfxlin <syfxlin@gmail.com>
2019-09-10 23:35:16 +0800

    docs: :pencil: Update LICENSE

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,22 @@
 module.exports = {
-    env: {
-        browser: true,
-        es6: true,
-        commonjs: true,
-        node: true
-    },
-    extends: ["eslint:recommended", "plugin:vue/essential"],
-    globals: {
-        Atomics: "readonly",
-        SharedArrayBuffer: "readonly"
-    },
-    parserOptions: {
-        ecmaVersion: 2018,
-        sourceType: "module"
-    },
-    plugins: ["vue"],
-    rules: {
-        "no-useless-escape": [0],
-        "no-unused-vars": [1]
-    }
+  env: {
+    browser: true,
+    es6: true,
+    commonjs: true,
+    node: true
+  },
+  extends: ['eslint:recommended', 'plugin:vue/essential'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: ['vue'],
+  rules: {
+    'no-useless-escape': [0],
+    'no-unused-vars': [1]
+  }
 };
