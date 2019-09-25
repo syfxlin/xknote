cd182f05d3b9761e59320242bbbd167607fa731e
syfxlin <syfxlin@gmail.com>
2019-09-25 22:49:01 +0800

    refactor: :art: Modify route group

--- routes/web.php
+++ routes/web.php
@@ -17,39 +17,35 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')
+    ->middleware('auth')
+    ->name('home');
 
-Route::get('/api/user', 'API\UserController@user')->middleware('auth');
+Route::prefix('api')
+    ->name('api.')
+    ->middleware('auth')
+    ->namespace('API')
+    ->group(function () {
+        Route::get('/user', 'UserController@user');
 
-Route::get('/api/admin/users', 'API\AdminController@getUser')->middleware('admin');
-Route::delete('/api/admin/users/{id}', 'API\AdminController@deleteUser')->middleware('admin');
-Route::post('/api/admin/users', 'API\AdminController@createUser')->middleware('admin');
+        Route::get('/admin/users', 'AdminController@getUser');
+        Route::delete('/admin/users/{id}', 'AdminController@deleteUser');
+        Route::post('/admin/users', 'AdminController@createUser');
 
-Route::get('/api/folders', 'API\FolderController@get')->middleware('auth');
-Route::post('/api/folders', 'API\FolderController@create')->middleware('auth');
-Route::delete('/api/folders', 'API\FolderController@delete')->middleware(
-    'auth'
-);
-Route::put('/api/folders', 'API\FolderController@rename')->middleware('auth');
-Route::put('/api/folders/rename', 'API\FolderController@rename')->middleware(
-    'auth'
-);
-Route::put('/api/folders/move', 'API\FolderController@move')->middleware(
-    'auth'
-);
-Route::get('/api/folders/only', 'API\FolderController@getOnly')->middleware(
-    'auth'
-);
-Route::get('/api/folders/flat', 'API\FolderController@getFlat')->middleware(
-    'auth'
-);
+        Route::get('/folders', 'FolderController@get');
+        Route::post('/folders', 'FolderController@create');
+        Route::delete('/folders', 'FolderController@delete');
+        Route::put('/folders', 'FolderController@rename');
+        Route::put('/folders/rename', 'FolderController@rename');
+        Route::put('/folders/move', 'FolderController@move');
+        Route::get('/folders/only', 'FolderController@getOnly');
+        Route::get('/folders/flat', 'FolderController@getFlat');
 
-Route::get('/api/notes', 'API\NoteController@get')->middleware('auth');
-Route::post('/api/notes', 'API\NoteController@create')->middleware('auth');
-Route::delete('/api/notes', 'API\NoteController@delete')->middleware('auth');
-Route::put('/api/notes', 'API\NoteController@edit')->middleware('auth');
-Route::put('/api/notes/rename', 'API\NoteController@rename')->middleware(
-    'auth'
-);
-Route::put('/api/notes/move', 'API\NoteController@move')->middleware('auth');
-Route::get('/api/notes/all', 'API\NoteController@getAll')->middleware('auth');
+        Route::get('/notes', 'NoteController@get');
+        Route::post('/notes', 'NoteController@create');
+        Route::delete('/notes', 'NoteController@delete');
+        Route::put('/notes', 'NoteController@edit');
+        Route::put('/notes/rename', 'NoteController@rename');
+        Route::put('/notes/move', 'NoteController@move');
+        Route::get('/notes/all', 'NoteController@getAll');
+    });
