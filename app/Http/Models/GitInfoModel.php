99d4783e8c82dc6a7bd72fdb3b3ba2cbd56f8633
syfxlin <syfxlin@gmail.com>
2019-10-24 17:39:52 +0800

    feat: :sparkles: Add create_git_info migration

---
+++ app/Http/Models/GitInfoModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class GitInfoModel extends Model
+{
+    protected $table = 'git_info';
+    public $timestamps = false;
+}
--- database/migrations/2019_10_24_093028_create_git_info_table.php
+++ database/migrations/2019_10_24_093028_create_git_info_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateGitInfoTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('git_info', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('username')->unique();
-            $table->string('nickname');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->bigIncrements('uid');
+            $table->string('git_name');
+            $table->string('git_email');
+            $table->string('git_password');
             $table->timestamps();
         });
     }
@@ -32,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('git_info');
     }
 }
