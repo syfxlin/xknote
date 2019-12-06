6de1e9613309a42109ef973a967afd8ee7b27499
syfxlin <syfxlin@gmail.com>
2019-12-06 11:36:06 +0800

    feat: :sparkles: Upgrade xkeditor@1.4.5(Move lang list to out setting)

--- resources/js/utils/otherConfig.js
+++ resources/js/utils/otherConfig.js
@@ -28,7 +28,23 @@ const otherConfig = {
     graffUrl: "static/",
     graffUpload: "http://test.ixk.me/upload.php",
     scrollMode: "javascript",
-    judge0API: "https://run-code.lincdn.top"
+    judge0API: "https://run-code.lincdn.top",
+    runCodeLangList: {
+      c: 1,
+      cpp: 2,
+      bash: 3,
+      csharp: 4,
+      go: 5,
+      java: 6,
+      node: 7,
+      php: 8,
+      python: 9,
+      python2: 10,
+      ruby: 11,
+      rust: 12,
+      scala: 13,
+      typescript: 14
+    }
   }
 };
 
